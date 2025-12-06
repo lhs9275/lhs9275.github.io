42f4f6697c135acd4452d397770ca01e69d18a83
Lee hyunsoo <98660600+lhs9275@users.noreply.github.com>
2025-12-06 17:31:31 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,15 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Hyunsoo Lee",
+  title: "Software Engineer",
+  description: "Portfolio website of Hyunsoo Lee",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "hslee9275@gmail.com",
+    github: "https://github.com/lhs9275",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+  skills: ["Javascript", "React", "Node.js", "Python", "JAVA", "Docker"],
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
