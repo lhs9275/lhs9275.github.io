0bae6b3ad029f7c8c431abde9692c130c4b52b77
Lee hyunsoo <98660600+lhs9275@users.noreply.github.com>
2025-12-06 17:22:06 +0900

    Update astro.config.mjs
    setting change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://lhs9275.github.io',
   vite: {
-    site: 'https://lhs9275.github.io',
     plugins: [tailwindcss()],
   },
 });
